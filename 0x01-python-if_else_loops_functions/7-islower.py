adb7805f2049552d53a5546e5bb73793aa4f32f4
Felipe Satizabal <1165@holbertonschool.com>
2019-12-10 19:10:30 +0000

    Fix

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,4 +3,6 @@ def islower(c):
         asciiValue = ord(c)
         if asciiValue >= 65 and asciiValue <= 90:
                 return False
+        elif asciiValue >= 48 and asciiValue <= 57:
+                return False
         return True
