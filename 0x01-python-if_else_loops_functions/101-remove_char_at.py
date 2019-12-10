e42562918221d7dab061ab4c57e0ccf53da1f36c
Felipe Satizabal <1165@holbertonschool.com>
2019-12-10 22:54:50 +0000

    Add 101

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -4,4 +4,4 @@ def remove_char_at(str, n):
         for i in range(0, len(str)):
                 if (i != n):
                         strCopy += str[i]
-                        return strCopy
+        return strCopy
