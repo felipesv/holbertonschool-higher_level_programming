224978a4fd9f34a16e043995709154516819d79f
Felipe Satizabal <1165@holbertonschool.com>
2019-12-12 20:16:09 +0000

    Fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,11 +5,13 @@ from calculator_1 import add, sub, mul, div
 if (__name__ == "__main__"):
     lenAr = len(sys.argv)
     if (lenAr != 4):
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("{:s}".
+              format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
         exit(1)
     opr = sys.argv[2]
     if (opr != "+" and opr != "-" and opr != "*" and opr != "/"):
-        print("Unknown operator. Available operators: +, -, * and /")
+        print("{:s}".
+              format("Unknown operator. Available operators: +, -, * and /"))
         exit(1)
 
     a = int(sys.argv[1])
@@ -18,8 +20,9 @@ if (__name__ == "__main__"):
     if (opr == "+"):
         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     if (opr == "-"):
-        print("{:d} + {:d} = {:d}".format(a, b, sub(a, b)))
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     if (opr == "*"):
-        print("{:d} + {:d} = {:d}".format(a, b, mul(a, b)))
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     if (opr == "/"):
-        print("{:d} + {:d} = {:d}".format(a, b, div(a, b)))
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    exit(0)
--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,1 @@
-#!/usr/bin/python3
-pythoniscool
+#!/usr/bin/python3 pythoniscool
