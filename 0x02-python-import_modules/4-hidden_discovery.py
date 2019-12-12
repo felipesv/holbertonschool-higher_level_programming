a0063c680a0ba3055e3586882165c8c12806c6ab
Felipe Satizabal <1165@holbertonschool.com>
2019-12-12 17:05:10 +0000

    fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
-arg = dir(hidden_4)
-for i in range(0, len(arg)):
-    if arg[i].find("__") == -1:
-        print(arg[i])
+if (__name__ == "__main__"):
+    arg = dir(hidden_4)
+    for i in range(0, len(arg)):
+        if arg[i].find("__") == -1:
+            print(arg[i])
