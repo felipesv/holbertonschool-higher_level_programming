9836698540dac8fdbe6cd98b33e2340d3a13f465
Felipe Satizabal <1165@holbertonschool.com>
2019-12-17 18:30:44 +0000

    fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,3 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_str = ""
-    for i in range(0, len(my_string)):
-        ascii_num = ord(my_string[i])
-        if ascii_num != 67 and ascii_num != 99:
-            new_str += my_string[i]
-    return new_str
+    return (my_string.translate({ord('c'): None})).translate({ord('C'): None})
