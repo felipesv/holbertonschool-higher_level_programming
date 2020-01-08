696c350608587b16853fae62637b597f217c90ab
Felipe Satizabal <1165@holbertonschool.com>
2020-01-08 13:41:13 -0500

    fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,10 @@ def safe_print_list(my_list=[], x=0):
     cnt = 0
     try:
         for i in range(x):
-            print("{}".format(my_list[i]), end="");
+            print("{}".format(my_list[i]), end="")
             cnt += 1
         print("")
         return cnt
     except IndexError:
         print("")
-        return cnt;
+        return cnt
