0cb467c4b433e4aad0c7e63c144c715037db372c
Felipe Satizabal <1165@holbertonschool.com>
2020-01-08 15:46:36 +0000

    add 0,1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    cnt = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="");
+            cnt += 1
+        print("")
+        return cnt
+    except IndexError:
+        print("")
+        return cnt;
