f5737daaf47f47a2b97e5339d401523b95e8627d
Felipe Satizabal <1165@holbertonschool.com>
2020-01-08 16:18:15 -0500

    fix

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except Exception:
+    except Exception as msj:
+        print("Exception: {}".format(msj), file=sys.stderr)
         return False
     return True
