fa269ad3cf185d43c8f5771738fc9f89f69bc95a
Felipe Satizabal <1165@holbertonschool.com>
2020-01-08 16:39:45 -0500

    fix

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,6 +4,6 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        fct(*args)
+        return fct(*args)
     except Exception as msj:
         print("Exception: {}". format(msj), file=sys.stderr)
