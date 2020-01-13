d7ef9c60a9ab18bcc807a3f4b784c6c9d3606b8b
felipesv <1165@holbertonschool.com>
2020-01-12 21:13:48 -0500

    add 5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+prints a text with 2 new lines after each charac . ? :
+text: text to print
+"""
+
+
+def text_indentation(text):
+    """
+    prints a text with 2 new lines after each charac . ? :
+    """
+    if type(text) != str:
+        raise TypeError("text must be a string")
+
+    text = text.rstrip()
+    _len = len(text)
+    _cnt = 0
+
+    while _cnt < _len and ord(text[_cnt]) == 32:
+        _cnt += 1
+
+    while _cnt < _len:
+        if text[_cnt] == "." or text[_cnt] == "?" or text[_cnt] == ":":
+            print("{:s}".format(text[_cnt]), end="\n\n")
+            while (_cnt + 1) < _len and ord(text[_cnt + 1]) == 32:
+                _cnt += 1
+        else:
+            print("{:s}".format(text[_cnt]), end="")
+
+        _cnt += 1
