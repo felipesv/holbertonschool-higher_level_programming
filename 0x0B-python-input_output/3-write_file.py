c995881e4d59c47282aa4bd994d7921a645829a8
Felipe Satizabal <1165@holbertonschool.com>
2020-01-21 12:13:56 -0500

    fix pep

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -8,8 +8,8 @@ def write_file(filename="", text=""):
     """
     writes a string to a text file and returns the number
     """
-    with open(filename, encoding="utf-8") as file:        
+    with open(filename, encoding="utf-8") as file:
         count = 0
-        for line in file.readlines():            
+        for line in file.readlines():
             count += 1 + len(line)
         return count
