f13adeca07be76a5a3ec74833ed590acbb487b4e
Felipe Satizabal <1165@holbertonschool.com>
2020-01-21 13:26:46 -0500

    add 4

---
+++ 0x0B-python-input_output/4-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+appends a string at the end of a text file
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    appends a string at the end of a text file
+    """
+    with open(filename, mode="a", encoding="utf-8") as file:
+        file.write(text)
+        return len(text)
