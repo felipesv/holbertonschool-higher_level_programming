ac2eb0ca297137f130505d838f28dc81f8be295b
Felipe Satizabal <1165@holbertonschool.com>
2020-03-02 14:14:16 -0500

    add 100

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- alter database to utf
+-- alter table to utf8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
