b45623fa402fddb6c01bc7194d359d586dbe6f1e
Felipe Satizabal <1165@holbertonschool.com>
2020-03-03 18:06:42 -0500

    fix to left join task 100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,5 +1,6 @@
 -- list all genres not linked to the show Dexter
-SELECT tv_genres.name
+-- two queries
+SELECT DISTINCT tv_genres.name
 FROM tv_genres
 LEFT JOIN tv_show_genres
 	ON tv_genres.id = tv_show_genres.show_id
