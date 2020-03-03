1aac1e1788491ad4897d9c906586a7345ec879d7
Felipe Satizabal <1165@holbertonschool.com>
2020-03-03 18:26:17 -0500

    fix 101

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -6,7 +6,7 @@ ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
 WHERE title NOT IN (
-    SELECT tv_genres.title
+    SELECT tv_shows.title
     FROM tv_genres
     LEFT JOIN tv_show_genres
     ON tv_genres.id = tv_show_genres.genre_id
