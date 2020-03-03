17f3db32ee89626e7a5912183d2ff477ac58ecbe
Felipe Satizabal <1165@holbertonschool.com>
2020-03-03 11:22:10 -0500

    add all task file, 0-5 task ended

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- create an user and grant the privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,2 @@
+-- create a table with a row not null
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
