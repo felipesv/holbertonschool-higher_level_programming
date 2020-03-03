e16fc796a9dc985b6bd4a17225c994b7a5c3757f
Felipe Satizabal <1165@holbertonschool.com>
2020-03-03 13:45:08 -0500

    fix 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
---lists all cities contained in the database hbtn_0d_usa.
-SELECT b.id, b.name, a.name
-FROM cities AS b
-JOIN states AS a
-ON a.id = b.state_id
-ORDER BY b.id ASC;
+-- select with two tables and a join
+-- 
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id;
