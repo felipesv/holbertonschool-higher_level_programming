d7f5026c8e06686fc271438f7dce3ffbb8b43949
felipesv <1165@holbertonschool.com>
2020-03-27 10:43:54 -0500

    add task 0, 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# connect to mysql and list table state
+
+# import modules
+import sys
+import MySQLdb
+
+# take arguments
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+# connect to the db
+db = MySQLdb.connect(
+    host="localhost",
+    user=username,
+    passwd=password,
+    db=database,
+    port=3306
+)
+
+# Cursor class
+cursor = db.cursor()
+# Exceute the query
+sql = "SELECT * FROM states WHERE name LIKE BINARY 'N%'ORDER BY states.id ASC"
+cursor.execute(sql)
+# fetches all and return list a tuples
+records = cursor.fetchall()
+# print each tuple
+for row in records:
+    print(row)
+cursor.close()
+db.close()
