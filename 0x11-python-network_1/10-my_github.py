cceb13adfb539a620e21bb3a8d05aa3bb449d6c6
felipesv <1165@holbertonschool.com>
2020-04-15 14:59:10 -0500

    fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,4 +8,7 @@ import sys
 if __name__ == '__main__':
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json()["id"])
+    try:
+        print(r.json()["id"])
+    except Exception:
+        print("None")
