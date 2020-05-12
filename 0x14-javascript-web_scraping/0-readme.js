f4c9453b962d834998dc391142feb8d58fdf27ac
felipesv <1165@holbertonschool.com>
2020-05-12 12:14:14 -0500

    fix 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,8 +3,10 @@
   Readme
 */
 const fs = require('fs');
-try {
-  console.log(fs.readFileSync(process.argv[2], 'utf-8'));
-} catch (error) {
-  console.log(error);
-}
+fs.readFile(process.argv[2], 'utf-8', function (error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
